24345cd61f2240b41b0cb5957d966aa761598867
BGehrels <github@gehrels>
2012-12-11 15:22:16 +0000

    Implemented HyperGraphStronglyConnectedComponents

--- benchmark/src/main/java/info/gehrels/diplomarbeit/hypergraphdb/HyperGraphDBStronglyConnectedComponents.java
+++ benchmark/src/main/java/info/gehrels/diplomarbeit/hypergraphdb/HyperGraphDBStronglyConnectedComponents.java
@@ -1,30 +1,44 @@
 package info.gehrels.diplomarbeit.hypergraphdb;
 
 import info.gehrels.diplomarbeit.AbstractStronglyConnectedComponentsCalculator;
+import info.gehrels.diplomarbeit.TransformingIteratorWrapper;
 import org.hypergraphdb.HGHandle;
+import org.hypergraphdb.HGQuery.hg;
+import org.hypergraphdb.HGValueLink;
 import org.hypergraphdb.HyperGraph;
 
-public class HyperGraphDBStronglyConnectedComponents extends AbstractStronglyConnectedComponentsCalculator<HyperGraph, HGHandle> {
+import java.util.List;
+
+public class HyperGraphDBStronglyConnectedComponents
+	extends AbstractStronglyConnectedComponentsCalculator<HyperGraph, HGHandle> {
+	public static void main(String[] args) throws Exception {
+		new HyperGraphDBStronglyConnectedComponents(
+			HyperGraphDBHelper.createHyperGraphDB("C:/Users/Benjamin/Desktop/hypergraphdb"))
+			.calculateStronglyConnectedComponents();
+	}
+
 	public HyperGraphDBStronglyConnectedComponents(HyperGraph hyperGraph) {
 		super(hyperGraph);
 	}
 
-	public void calculateStronglyConnectedComponents() {
-		throw new UnsupportedOperationException();
-	}
-
 	@Override
 	protected Iterable<HGHandle> getAllNodes() {
-		throw new UnsupportedOperationException();
+		return hg.findAll(graphDB, hg.type(Long.class));
 	}
 
 	@Override
 	protected long getNodeName(HGHandle node) {
-		throw new UnsupportedOperationException();
+		return graphDB.get(node);
 	}
 
 	@Override
 	protected Iterable<HGHandle> getOutgoingIncidentNodes(HGHandle node) throws Exception {
-		throw new UnsupportedOperationException();
+		List<HGValueLink> all = graphDB.getAll(hg.incidentAt(node, 0));
+		return new TransformingIteratorWrapper<HGValueLink, HGHandle>(all.iterator()) {
+			@Override
+			protected HGHandle calculateNext(HGValueLink next) {
+				return next.getTargetAt(1);
+			}
+		};
 	}
 }
