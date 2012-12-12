d5ad366181a1460e5e07961ab1ad691f7e18a749
BGehrels <github@gehrels>
2012-12-12 13:28:13 +0000

    HyperGraphDBHelper.java will not register a shutdown hook any more, since it deadlocks and there already is some in HGEnvironment

--- benchmark/src/main/java/info/gehrels/diplomarbeit/hypergraphdb/HyperGraphDBHelper.java
+++ benchmark/src/main/java/info/gehrels/diplomarbeit/hypergraphdb/HyperGraphDBHelper.java
@@ -5,25 +5,7 @@ import org.hypergraphdb.HyperGraph;
 import java.io.IOException;
 
 public class HyperGraphDBHelper {
-
 	static HyperGraph createHyperGraphDB(String dbPath) throws IOException {
-		HyperGraph graphDb = new HyperGraph(dbPath);
-		registerShutdownHook(graphDb);
-		return graphDb;
+		return new HyperGraph(dbPath);
 	}
-
-	private static void registerShutdownHook(final HyperGraph graphDb) {
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			@Override
-			public void run() {
-				try {
-					sleep(50); // Allow the other shutdown hooks to complete first
-				} catch (InterruptedException e) {
-					Thread.currentThread().interrupt();
-				}
-				graphDb.close();
-			}
-		});
-	}
-
 }
