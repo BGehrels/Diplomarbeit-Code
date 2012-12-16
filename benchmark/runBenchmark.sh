ea6c8c929d328557c933d17484adf53c7fcc21a1
BGehrels <github@gehrels>
2012-12-16 20:15:09 +0000

    refactorings; now using the runnable  jar-with-dependencies; Printing out the stderr results after logging them; exiting, if a benchmark step fails; added hypergraphdb to the benchmark; comparing the logs to find inconsistent results between different databases;

--- benchmark/runBenchmark.sh
+++ benchmark/runBenchmark.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 shopt -s nullglob
-
+declare -a algos=(import readWholeGraph calcSCC calcFoF calcCommonFriends calcRegularPathQueries)
+declare -a dbs=(flockdb neo4j hypergraphdb)
 
 function runBenchmarkStep() {
-	JAVACP="target/classes;C:/Users/Benjamin/.m2/repository/org/neo4j/neo4j/1.8.RC1/neo4j-1.8.RC1.jar;C:/Users/Benjamin/.m2/repository/org/neo4j/neo4j-kernel/1.8.RC1/neo4j-kernel-1.8.RC1.jar;C:/Users/Benjamin/.m2/repository/org/apache/geronimo/specs/geronimo-jta_1.1_spec/1.1.1/geronimo-jta_1.1_spec-1.1.1.jar;C:/Users/Benjamin/.m2/repository/org/neo4j/neo4j-lucene-index/1.8.RC1/neo4j-lucene-index-1.8.RC1.jar;C:/Users/Benjamin/.m2/repository/org/apache/lucene/lucene-core/3.5.0/lucene-core-3.5.0.jar;C:/Users/Benjamin/.m2/repository/org/neo4j/neo4j-graph-algo/1.8.RC1/neo4j-graph-algo-1.8.RC1.jar;C:/Users/Benjamin/.m2/repository/org/neo4j/neo4j-udc/1.8.RC1/neo4j-udc-1.8.RC1.jar;C:/Users/Benjamin/.m2/repository/org/neo4j/neo4j-graph-matching/1.8.RC1/neo4j-graph-matching-1.8.RC1.jar;C:/Users/Benjamin/.m2/repository/org/neo4j/neo4j-cypher/1.8.RC1/neo4j-cypher-1.8.RC1.jar;C:/Users/Benjamin/.m2/repository/org/scala-lang/scala-library/2.9.1-1/scala-library-2.9.1-1.jar;C:/Users/Benjamin/.m2/repository/org/neo4j/neo4j-jmx/1.8.RC1/neo4j-jmx-1.8.RC1.jar;C:/Users/Benjamin/.m2/repository/com/google/guava/guava/13.0-rc2/guava-13.0-rc2.jar;C:/Users/Benjamin/.m2/repository/org/apache/commons/commons-io/1.3.2/commons-io-1.3.2.jar;C:/Users/Benjamin/.m2/repository/info/gehrels/FlockDBClient/0.1-SNAPSHOT/FlockDBClient-0.1-SNAPSHOT.jar;C:/Users/Benjamin/.m2/repository/org/apache/thrift/libthrift/0.8.0/libthrift-0.8.0.jar;C:/Users/Benjamin/.m2/repository/org/slf4j/slf4j-api/1.7.2/slf4j-api-1.7.2.jar;C:/Users/Benjamin/.m2/repository/commons-lang/commons-lang/2.5/commons-lang-2.5.jar;C:/Users/Benjamin/.m2/repository/org/apache/httpcomponents/httpclient/4.1.2/httpclient-4.1.2.jar;C:/Users/Benjamin/.m2/repository/org/apache/httpcomponents/httpcore/4.1.3/httpcore-4.1.3.jar;C:/Users/Benjamin/.m2/repository/commons-logging/commons-logging/1.1.1/commons-logging-1.1.1.jar;C:/Users/Benjamin/.m2/repository/commons-codec/commons-codec/1.4/commons-codec-1.4.jar;C:/Users/Benjamin/.m2/repository/org/slf4j/slf4j-nop/1.7.2/slf4j-nop-1.7.2.jar;C:/Users/Benjamin/.m2/repository/mysql/mysql-connector-java/5.1.21/mysql-connector-java-5.1.21.jar"
 	LOGFILENAME="logs/$1-$2-$3.stdout"
 	TIMEFILENAME="logs/$1-$2-$3.stderr"
 
@@ -12,7 +12,15 @@ function runBenchmarkStep() {
 	then
 		echo "running $3 for $2 on $1"
 
-		java -Xmx1g -classpath "$JAVACP" info.gehrels.diplomarbeit.RunBenchmarkStep geoff/$1 $2 $3 1> "$LOGFILENAME" 2> "$TIMEFILENAME"
+		java -Xmx1g -jar target/benchmark-1.0-jar-with-dependencies.jar geoff/$1 $2 $3 1> "$LOGFILENAME" 2> "$TIMEFILENAME"
+		RESULT = $?
+		cat $TIMEFILENAME
+
+		if [[ $RESULT != 0 ]] ; then
+			echo "ERROR"
+			exit
+		fi
+
 		rebootToClearCaches
 	fi
 
@@ -29,6 +37,7 @@ function clearAllDatabaseTmpFiles() {
 	then
 		echo "clearing tmp files for $1"
 		rm -Rf neo4jDB
+		rm -Rf hyperGraphDB
 		echo "DROP DATABASE edges_development" | mysql -u root
 		echo "DROP DATABASE flockdb_development" | mysql -u root
 
@@ -43,21 +52,47 @@ function clearAllDatabaseTmpFiles() {
 }
 
 function runBenchmark() {
-    runBenchmarkStep $1 $2 "import"
-    runBenchmarkStep $1 $2 "readWholeGraph"
-	runBenchmarkStep $1 $2 "calcSCC"
-    runBenchmarkStep $1 $2 "calcFoF"
-    runBenchmarkStep $1 $2 "calcCommonFriends"
-    runBenchmarkStep $1 $2 "calcRegularPathQueries"
+	for algo in ${algos[@]}
+	do
+	    runBenchmarkStep $1 $2 $algo
+	done
+}
+
+
+function compareLogs() {
+	# args: 1: dataFileName, 2: db1, 3: db2
+	for algo in ${algos[@]}
+	do
+		compareLogsForAlgo $1 $algo $2 $3
+	done
+}
+
+function compareLogsForAlgo() {
+	# args: 1: dataFileName, 2: algo, 3: db1, 4: db2
+	sort logs/$1-$3-$2.stdout | grep -v "checkpoint .*:0" > logs/$1-$3-$2.sorted
+	sort logs/$1-$4-$2.stdout | grep -v "checkpoint .*:0" > logs/$1-$4-$2.sorted
+	diff -q logs/$1-$3-$2.sorted logs/$1-$4-$2.sorted
+	if [[ $? != 0 ]] ; then
+        echo logs/$1-$3-$2.sorted and logs/$1-$4-$2.sorted were different
+        exit
+    fi
 }
 
 for f in geoff/*.geoff
 do
 	echo "Running benchmark for $f";
 
-	for db in flockdb neo4j
+	for db in ${dbs[@]}
 	do
 		runBenchmark `basename $f` $db
+	done
+
+	for db1 in ${dbs[@]}
+	do
+		for db2 in ${dbs[@]}
+        do
+        	compareLogs `basename $f` $db1 $db2
+        done
 	done
 
 
