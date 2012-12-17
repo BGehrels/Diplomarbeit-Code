f02edb735d60de5c62516fb99ab25be413a458f1
BGehrels <github@gehrels>
2012-12-17 17:41:33 +0000

    Precompiling the select statements for HyperGraphRegularPathQuery.java

--- benchmark/src/main/java/info/gehrels/diplomarbeit/hypergraphdb/HyperGraphRegularPathQuery.java
+++ benchmark/src/main/java/info/gehrels/diplomarbeit/hypergraphdb/HyperGraphRegularPathQuery.java
@@ -4,24 +4,35 @@ import com.twitter.flockdb.thrift.FlockException;
 import info.gehrels.diplomarbeit.AbstractRegularPathQuery;
 import info.gehrels.diplomarbeit.Measurement;
 import org.hypergraphdb.HGHandle;
+import org.hypergraphdb.HGQuery;
 import org.hypergraphdb.HGQuery.hg;
 import org.hypergraphdb.HGSearchResult;
-import org.hypergraphdb.HGValueLink;
 import org.hypergraphdb.HyperGraph;
 
 import java.io.IOException;
 
 import static info.gehrels.diplomarbeit.hypergraphdb.HyperGraphDBHelper.createHyperGraphDB;
 import static java.lang.Long.parseLong;
+import static org.hypergraphdb.HGQuery.hg.and;
+import static org.hypergraphdb.HGQuery.hg.apply;
+import static org.hypergraphdb.HGQuery.hg.eq;
+import static org.hypergraphdb.HGQuery.hg.incidentAt;
+import static org.hypergraphdb.HGQuery.hg.orderedLink;
+import static org.hypergraphdb.HGQuery.hg.targetAt;
+import static org.hypergraphdb.HGQuery.hg.var;
 
 public class HyperGraphRegularPathQuery extends AbstractRegularPathQuery {
 	private final HyperGraph database;
+	private HGQuery<HGHandle> aIdQuery;
+	private final HGQuery<HGHandle> incomingIncidentNodesQueryL2;
+	private final HGQuery<HGHandle> incomingIncidentNodesQueryL3;
 
 	public static void main(final String[] args) throws Exception {
 		Measurement.measure(new Measurement<Void>() {
 			@Override
 			public void execute(Void database) throws Exception {
-				new HyperGraphRegularPathQuery(createHyperGraphDB(args[0]), parseLong(args[1])).calculateRegularPaths();
+				new HyperGraphRegularPathQuery(createHyperGraphDB(args[0]), parseLong(args[1]))
+					.calculateRegularPaths();
 			}
 		});
 	}
@@ -29,6 +40,28 @@ public class HyperGraphRegularPathQuery extends AbstractRegularPathQuery {
 	public HyperGraphRegularPathQuery(HyperGraph database, long maxNodeId) {
 		super(maxNodeId);
 		this.database = database;
+
+		this.aIdQuery = hg.make(HGHandle.class, database).compile(eq(var("aId")));
+		this.incomingIncidentNodesQueryL2 = hg.make(HGHandle.class, database)
+			.compile(
+				apply(
+					targetAt(database, 0),
+					and(
+						eq("L2"),
+						incidentAt(var("node", HGHandle.class), 1)
+					)
+				)
+			);
+		this.incomingIncidentNodesQueryL3 = hg.make(HGHandle.class, database)
+			.compile(
+				apply(
+					targetAt(database, 0),
+					and(
+						eq("L3"),
+						incidentAt(var("node", HGHandle.class), 1)
+					)
+				)
+			);
 	}
 
 	// TODO: Impliziert hg.eq("L3") hg.type(String.class)?
@@ -42,24 +75,41 @@ public class HyperGraphRegularPathQuery extends AbstractRegularPathQuery {
 		// RETURN a.name, b.name, c.name
 		// ORDER BY a.name, b.name, c.name
 
-		HGHandle a = database.findOne(hg.eq((long) id1));
-		HGSearchResult<HGHandle> caLinks = database.find(hg.and(hg.eq("L3"), hg.incidentAt(a, 1)));
-		while (caLinks.hasNext()) {
-			HGHandle c = ((HGValueLink) database.get(caLinks.next())).getTargetAt(0);
-			HGSearchResult<HGHandle> bcLinks = database.find(hg.and(hg.eq("L2"), hg.incidentAt(c, 1)));
+		HGHandle a = aIdQuery.var("aId", (long) id1).findOne();
+		HGSearchResult<HGHandle> cNodeCandidates = findIncomingIncidentNodes(a, "L3");
+		while (cNodeCandidates.hasNext()) {
+			HGHandle cNodeCandidate = cNodeCandidates.next();
+			HGSearchResult<HGHandle> bNodeCandidates = findIncomingIncidentNodes(cNodeCandidate, "L2");
 
-			while (bcLinks.hasNext()) {
-				HGHandle b = ((HGValueLink) database.get(bcLinks.next())).getTargetAt(0);
-				HGHandle abLink = database.findOne(hg.and(hg.eq("L1"), hg.incidentAt(a, 0), hg.incidentAt(b, 1)));
+			while (bNodeCandidates.hasNext()) {
+				HGHandle bNodeCandidate = bNodeCandidates.next();
+				HGHandle abLink = database.findOne(and(eq("L1"), orderedLink(a, bNodeCandidate)));
 				if (abLink != null) {
-					printHit((Long) database.get(a), (Long) database.get(b), (Long)database.get(c));
+					printHit((Long) database.get(a), (Long) database.get(bNodeCandidate),
+					         (Long) database.get(cNodeCandidate));
 				}
 
 			}
 
-			bcLinks.close();
+			bNodeCandidates.close();
 		}
 
-		caLinks.close();
+		cNodeCandidates.close();
 	}
+
+	private HGSearchResult<HGHandle> findIncomingIncidentNodes(HGHandle node, String edgeLabel) {
+		switch (edgeLabel) {
+			case "L2":
+				return incomingIncidentNodesQueryL2
+					.var("node", node)
+					.execute();
+			case "L3":
+				return incomingIncidentNodesQueryL3
+					.var("node", node)
+					.execute();
+			default:
+				throw new IllegalArgumentException();
+		}
+	}
+
 }
