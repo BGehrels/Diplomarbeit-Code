bc9d4cf7047eb3f18d7cdfb64b84a37c77114026
BGehrels <benjamin@gehrels>
2013-02-11 15:54:05 +0100

    Added correct RAM sizes for Neo4j

--- benchmark/src/main/java/info/gehrels/diplomarbeit/neo4j/Neo4jHelper.java
+++ benchmark/src/main/java/info/gehrels/diplomarbeit/neo4j/Neo4jHelper.java
@@ -9,11 +9,11 @@ import java.util.Map;
 public class Neo4jHelper {
 	static GraphDatabaseService createNeo4jDatabase(String dbPath) {
 		Map<String, String> config = new HashMap<>();
-		config.put("neostore.nodestore.db.mapped_memory", "xM");
-		config.put("neostore.relationshipstore.db.mapped_memory", "xM");
-		config.put("neostore.propertystore.db.mapped_memory", "xM");
-		config.put("neostore.propertystore.db.strings.mapped_memory", "xM");
-		config.put("neostore.propertystore.db.arrays.mapped_memory", "xM");
+		config.put("neostore.nodestore.db.mapped_memory", "100M");
+		config.put("neostore.relationshipstore.db.mapped_memory", "4G");
+		config.put("neostore.propertystore.db.mapped_memory", "500M");
+		config.put("neostore.propertystore.db.strings.mapped_memory", "1M");
+		config.put("neostore.propertystore.db.arrays.mapped_memory", "1M");
 
 		GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(dbPath).setConfig(config)
 			.newGraphDatabase();
