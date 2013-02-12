abe12baf183959e560716c2c8d5a16a015b4f927
BGehrels <github@gehrels>
2013-02-12 19:20:02 +0100

    HyperGraphDBHelper now uses orderedLink as condition to be able to use ByTargetIndices. Therefore we can not use precompiled queries anymore, but the do not work well in nested loop/recursion enviroments anyway.

--- benchmark/src/main/java/info/gehrels/diplomarbeit/hypergraphdb/HyperGraphCommonFriends.java
+++ benchmark/src/main/java/info/gehrels/diplomarbeit/hypergraphdb/HyperGraphCommonFriends.java
@@ -6,58 +6,55 @@ import org.hypergraphdb.HGHandle;
 import org.hypergraphdb.HGQuery;
 import org.hypergraphdb.HGSearchResult;
 import org.hypergraphdb.HyperGraph;
-
 import java.util.HashSet;
 import java.util.Set;
-
 import static info.gehrels.diplomarbeit.Measurement.measure;
-import static info.gehrels.diplomarbeit.hypergraphdb.HyperGraphDBHelper.createGetFriendNodesQuery;
+import static info.gehrels.diplomarbeit.hypergraphdb.HyperGraphDBHelper.getFriendNodes;
 import static info.gehrels.diplomarbeit.hypergraphdb.HyperGraphDBHelper.createGetNodeByIdQuery;
 import static info.gehrels.diplomarbeit.hypergraphdb.HyperGraphDBHelper.createHyperGraphDB;
 import static java.lang.Long.parseLong;
 
+
 public class HyperGraphCommonFriends extends AbstractCommonFriends {
-	private final HyperGraph database;
-	private final HGQuery<HGHandle> findNodeById;
-	private final HGQuery<HGHandle> findFriendsByNode;
+  private final HyperGraph database;
+  private final HGQuery<HGHandle> findNodeById;
 
-	public static void main(final String[] args) throws Exception {
-		measure(new Measurement<Void>() {
-			@Override
-			public void execute(Void database) throws Exception {
-				new HyperGraphCommonFriends(createHyperGraphDB(args[0]), parseLong(args[1])).calculateCommonFriends();
-			}
-		});
-	}
+  public static void main(final String[] args) throws Exception {
+    measure(new Measurement<Void>() {
+        @Override
+        public void execute(Void database) throws Exception {
+          new HyperGraphCommonFriends(createHyperGraphDB(args[0]), parseLong(args[1])).calculateCommonFriends();
+        }
+      });
+  }
 
-	public HyperGraphCommonFriends(HyperGraph database, long maxNodeId) {
-		super(maxNodeId);
-		this.database = database;
-		this.findNodeById = createGetNodeByIdQuery(database);
-		this.findFriendsByNode = createGetFriendNodesQuery(database, "L1", true);
+  public HyperGraphCommonFriends(HyperGraph database, long maxNodeId) {
+    super(maxNodeId);
+    this.database = database;
+    this.findNodeById = createGetNodeByIdQuery(database);
 
-	}
+  }
 
-	@Override
-	protected void calculateCommonFriends(int id1, int id2) throws Exception {
-		Set<HGHandle> friendsOf1 = getFriendsOf(id1);
-		Set<HGHandle> friendsOf2 = getFriendsOf(id2);
+  @Override
+  protected void calculateCommonFriends(int id1, int id2) throws Exception {
+    Set<HGHandle> friendsOf1 = getFriendsOf(id1);
+    Set<HGHandle> friendsOf2 = getFriendsOf(id2);
 
-		friendsOf1.retainAll(friendsOf2);
+    friendsOf1.retainAll(friendsOf2);
 
-		for (HGHandle hgHandle : friendsOf1) {
-			printCommonFriend(id1, id2, (Long) database.get(hgHandle));
-		}
-	}
+    for (HGHandle hgHandle : friendsOf1) {
+      printCommonFriend(id1, id2, (Long) database.get(hgHandle));
+    }
+  }
 
-	private Set<HGHandle> getFriendsOf(long id1) {
-		HGHandle node1 = findNodeById.var("id", id1).findOne();
-		HGSearchResult<HGHandle> l1 = findFriendsByNode.var("node", node1).execute();
-		Set<HGHandle> friendNodesOfNode1 = new HashSet<>();
-		while (l1.hasNext()) {
-			friendNodesOfNode1.add(l1.next());
-		}
-		l1.close();
-		return friendNodesOfNode1;
-	}
+  private Set<HGHandle> getFriendsOf(long id1) {
+    HGHandle node1 = findNodeById.var("id", id1).findOne();
+    HGSearchResult<HGHandle> l1 = getFriendNodes(database, "L1", node1, true);
+    Set<HGHandle> friendNodesOfNode1 = new HashSet<>();
+    while (l1.hasNext()) {
+      friendNodesOfNode1.add(l1.next());
+    }
+    l1.close();
+    return friendNodesOfNode1;
+  }
 }
--- benchmark/src/main/java/info/gehrels/diplomarbeit/hypergraphdb/HyperGraphDBHelper.java
+++ benchmark/src/main/java/info/gehrels/diplomarbeit/hypergraphdb/HyperGraphDBHelper.java
@@ -1,18 +1,10 @@
 package info.gehrels.diplomarbeit.hypergraphdb;
 
-import org.hypergraphdb.HGConfiguration;
-import org.hypergraphdb.HGEnvironment;
-import org.hypergraphdb.HGHandle;
-import org.hypergraphdb.HGQuery;
-import org.hypergraphdb.HyperGraph;
+import org.hypergraphdb.*;
+
 import java.io.IOException;
-import static org.hypergraphdb.HGQuery.hg.and;
-import static org.hypergraphdb.HGQuery.hg.apply;
-import static org.hypergraphdb.HGQuery.hg.eq;
-import static org.hypergraphdb.HGQuery.hg.incidentAt;
-import static org.hypergraphdb.HGQuery.hg.make;
-import static org.hypergraphdb.HGQuery.hg.targetAt;
-import static org.hypergraphdb.HGQuery.hg.var;
+
+import static org.hypergraphdb.HGQuery.hg.*;
 
 
 public class HyperGraphDBHelper {
@@ -27,18 +19,15 @@ public class HyperGraphDBHelper {
     return make(HGHandle.class, database).compile(eq(var("id")));
   }
 
-  static HGQuery<HGHandle> createGetFriendNodesQuery(HyperGraph database, String edgeLabel, boolean outgoing) {
-    // TODO: Mal ausprobieren, ob orderedLink(anyHandle(), var("node", HGHandle.class)) schneller ist, wenn es mit
-    // TODO: einem TargetAt index kombiniert wird. Funzt aber nut, wenn keine var() benutzt wird, sondern "echtes"
-    // TODO: handle. Eventuell könnte mensch auch einen CompositeIndex zusammen mit einer IndexCondition nutzen?
-    // TODO: Selbiges gitl für HyperGraphDBStronglyConnectedComponents.getOutgoingIncidentNodes()
-    return make(HGHandle.class, database).compile(
+  static HGSearchResult<HGHandle> getFriendNodes(HyperGraph database, String edgeLabel, HGHandle handle, boolean outgoing) {
+    return database.find(
       apply(
         targetAt(database, outgoing ? 1 : 0),
         and(
           eq(edgeLabel),
-          incidentAt(
-            var("node", HGHandle.class),
-            outgoing ? 0 : 1))));
+          outgoing ? orderedLink(handle, anyHandle()) : orderedLink(anyHandle(), handle)
+        )
+      )
+    );
   }
 }
