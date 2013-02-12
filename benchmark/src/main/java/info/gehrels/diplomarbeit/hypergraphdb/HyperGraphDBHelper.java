855a2185772169764321bb6323591032101d647d
BGehrels <github@gehrels>
2013-02-12 18:31:00 +0100

    Extended the todo comment

--- benchmark/src/main/java/info/gehrels/diplomarbeit/hypergraphdb/HyperGraphDBHelper.java
+++ benchmark/src/main/java/info/gehrels/diplomarbeit/hypergraphdb/HyperGraphDBHelper.java
@@ -5,9 +5,7 @@ import org.hypergraphdb.HGEnvironment;
 import org.hypergraphdb.HGHandle;
 import org.hypergraphdb.HGQuery;
 import org.hypergraphdb.HyperGraph;
-
 import java.io.IOException;
-
 import static org.hypergraphdb.HGQuery.hg.and;
 import static org.hypergraphdb.HGQuery.hg.apply;
 import static org.hypergraphdb.HGQuery.hg.eq;
@@ -16,34 +14,31 @@ import static org.hypergraphdb.HGQuery.hg.make;
 import static org.hypergraphdb.HGQuery.hg.targetAt;
 import static org.hypergraphdb.HGQuery.hg.var;
 
+
 public class HyperGraphDBHelper {
-	static HyperGraph createHyperGraphDB(String dbPath) throws IOException {
-		HGConfiguration config = new HGConfiguration();
-		config.setTransactional(false);
-		config.setUseSystemAtomAttributes(false);
-		return HGEnvironment.get(dbPath, config);
-	}
+  static HyperGraph createHyperGraphDB(String dbPath) throws IOException {
+    HGConfiguration config = new HGConfiguration();
+    config.setTransactional(false);
+    config.setUseSystemAtomAttributes(false);
+    return HGEnvironment.get(dbPath, config);
+  }
 
-	static HGQuery<HGHandle> createGetNodeByIdQuery(HyperGraph database) {
-		return make(HGHandle.class, database).compile(eq(var("id")));
-	}
+  static HGQuery<HGHandle> createGetNodeByIdQuery(HyperGraph database) {
+    return make(HGHandle.class, database).compile(eq(var("id")));
+  }
 
-	static HGQuery<HGHandle> createGetFriendNodesQuery(HyperGraph database, String edgeLabel, boolean outgoing) {
-		// TODO: Mal ausprobieren, ob orderedLink(anyHandle(), var("node", HGHandle.class)) schneller ist, wenn es mit
-		// TODO: einem TargetAt index kombiniert wird. Funzt aber nut, wenn keine var() benutzt wird, sondern "echtes"
-		// TODO: handle. Eventuell könnte mensch auch einen CompositeIndex zusammen mit einer IndexCondition nutzen?
-		return make(HGHandle.class, database)
-			.compile(
-				apply(
-					targetAt(database, outgoing ? 1 : 0),
-					and(
-						eq(edgeLabel),
-						incidentAt(
-							var("node", HGHandle.class),
-							outgoing ? 0 : 1
-						)
-					)
-				)
-			);
-	}
+  static HGQuery<HGHandle> createGetFriendNodesQuery(HyperGraph database, String edgeLabel, boolean outgoing) {
+    // TODO: Mal ausprobieren, ob orderedLink(anyHandle(), var("node", HGHandle.class)) schneller ist, wenn es mit
+    // TODO: einem TargetAt index kombiniert wird. Funzt aber nut, wenn keine var() benutzt wird, sondern "echtes"
+    // TODO: handle. Eventuell könnte mensch auch einen CompositeIndex zusammen mit einer IndexCondition nutzen?
+    // TODO: Selbiges gitl für HyperGraphDBStronglyConnectedComponents.getOutgoingIncidentNodes()
+    return make(HGHandle.class, database).compile(
+      apply(
+        targetAt(database, outgoing ? 1 : 0),
+        and(
+          eq(edgeLabel),
+          incidentAt(
+            var("node", HGHandle.class),
+            outgoing ? 0 : 1))));
+  }
 }
