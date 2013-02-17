c2b1cb8b006bc20b76867611cda38a551139f1a8
Benjamin Gehrels <github@gehrels>
2013-02-17 18:22:55 +0100

    Neo4j Common Friends wieder in Cypher implementiert, um Bias zu vermeiden

--- benchmark/src/main/java/info/gehrels/diplomarbeit/neo4j/Neo4jCommonFriends.java
+++ benchmark/src/main/java/info/gehrels/diplomarbeit/neo4j/Neo4jCommonFriends.java
@@ -4,29 +4,16 @@ import info.gehrels.diplomarbeit.AbstractCommonFriends;
 import info.gehrels.diplomarbeit.Measurement;
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
-import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.DynamicRelationshipType;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Path;
-import org.neo4j.graphdb.PathExpander;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.traversal.BranchState;
-import org.neo4j.graphdb.traversal.Evaluation;
-import org.neo4j.graphdb.traversal.Evaluator;
 import org.neo4j.helpers.collection.IteratorUtil;
 import org.neo4j.helpers.collection.MapUtil;
-
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
 
 import static info.gehrels.diplomarbeit.Measurement.measure;
 import static info.gehrels.diplomarbeit.neo4j.Neo4jHelper.createNeo4jDatabase;
 import static info.gehrels.diplomarbeit.neo4j.Neo4jImporter.NAME_KEY;
 import static info.gehrels.diplomarbeit.neo4j.Neo4jImporter.NODE_INDEX_NAME;
 import static java.lang.Integer.parseInt;
-import static org.neo4j.kernel.Traversal.traversal;
 
 public class Neo4jCommonFriends extends AbstractCommonFriends {
 	public static final DynamicRelationshipType L1 = DynamicRelationshipType.withName("L1");
@@ -49,86 +36,16 @@ public class Neo4jCommonFriends extends AbstractCommonFriends {
 
 	@Override
 	protected void calculateCommonFriends(int id1, int id2) {
-		calculateCommonFriendsUsingTwoTraversals(id1, id2);
-	}
-
-	private void calculateCommonFriendsUsingTwoTraversals(int id1, int id2) {
-		Node id1Node = graphDB.index().forNodes(NODE_INDEX_NAME).get(NAME_KEY, id1).next();
-		final Node id2Node = graphDB.index().forNodes(NODE_INDEX_NAME).get(NAME_KEY, id2)
-			.next();
-
-		Set<Long> id1Friends = new TreeSet<>();
-		for (Relationship rel : id1Node.getRelationships(L1, Direction.OUTGOING)) {
-			id1Friends.add((Long) rel.getEndNode().getProperty(Neo4jImporter.NAME_KEY));
-		}
-
-		Set<Long> id2Friends = new TreeSet<>();
-		for (Relationship rel : id2Node.getRelationships(L1, Direction.OUTGOING)) {
-			id2Friends.add((Long) rel.getEndNode().getProperty(Neo4jImporter.NAME_KEY));
-		}
-
-		id1Friends.retainAll(id2Friends);
-		for (Long id1Friend : id1Friends) {
-			printCommonFriend(id1, id2, id1Friend);
-		}
-	}
-
-	private void calculateCommonFriendsUsingPureTraversal(int id1, int id2) {
-		Node id1Node = graphDB.index().forNodes(NODE_INDEX_NAME).get(NAME_KEY, id1).next();
-		final Node id2Node = graphDB.index().forNodes(NODE_INDEX_NAME).get(NAME_KEY, id2)
-			.next();
-
-
-		Iterable<Path> pathTraversal =
-			traversal()
-				.breadthFirst()
-				.expand(new PathExpander<Object>() {
-					@Override
-					public Iterable<Relationship> expand(Path path, BranchState<Object> state) {
-						return path.length() == 0 ?
-							path.endNode().getRelationships(L1, Direction.OUTGOING) :
-							path.endNode().getRelationships(L1, Direction.INCOMING);
-					}
-
-					@Override
-					public PathExpander<Object> reverse() {
-						throw new UnsupportedOperationException();
-					}
-				})
-				.evaluator(new Evaluator() {
-					public Evaluation evaluate(Path path) {
-						if (path.length() < 2) {
-							return Evaluation.EXCLUDE_AND_CONTINUE;
-						}
-
-						if (path.endNode().equals(id2Node)) {
-							return Evaluation.INCLUDE_AND_PRUNE;
-						}
-
-						return Evaluation.EXCLUDE_AND_PRUNE;
-					}
-				})
-				.traverse(id1Node);
-
-		for (Path path : pathTraversal) {
-			Iterator<Node> iterator = path.nodes().iterator();
-			iterator.next();
-
-			printCommonFriend(id1, id2, (Long) iterator.next().getProperty(NAME_KEY));
-		}
-	}
-
-	private void calculateCommonFriendsUsingCypher(long id1, long id2) {
 		ExecutionEngine cypher = new ExecutionEngine(graphDB);
 		ExecutionResult result = cypher.execute("start " +
 		                                        "n=node:" + NODE_INDEX_NAME + "(" + NAME_KEY + "={id1}), " +
 		                                        "m=node:" + NODE_INDEX_NAME + "(" + NAME_KEY + "={id2}) " +
 		                                        "match m-[:L1]->x<-[:L1]-n " +
 		                                        "return x.name as x ",
-		                                        MapUtil.<String, Object>genericMap("id1", id1, "id2", id2));
+		                                        MapUtil.<String, Object>genericMap("id1", (long) id1, "id2", (long) id2));
 
 		for (Object x : IteratorUtil.asIterable(result.columnAs("x"))) {
-			printCommonFriend(id1, id2, (Long) x);
+			printCommonFriend((long) id1, (long) id2, (Long) x);
 		}
 	}
 
