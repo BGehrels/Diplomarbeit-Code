c7fcab11f5a285881c73d065345c94cfa319db8c
BGehrels <github@gehrels>
2013-02-20 18:13:38 +0100

    renamed the node Parameter of the AbstractImporter to better reflect what it is

--- benchmark/src/main/java/info/gehrels/diplomarbeit/AbstractImporter.java
+++ benchmark/src/main/java/info/gehrels/diplomarbeit/AbstractImporter.java
@@ -2,24 +2,25 @@ package info.gehrels.diplomarbeit;
 
 import java.io.FileInputStream;
 
+
 public abstract class AbstractImporter {
-	private final FileInputStream inputStream;
+  private final FileInputStream inputStream;
 
-	public AbstractImporter(String sourceFile) throws Exception {
-		this.inputStream = new FileInputStream(sourceFile);
-	}
+  public AbstractImporter(String sourceFile) throws Exception {
+    this.inputStream = new FileInputStream(sourceFile);
+  }
 
-	public final void importNow() throws Exception {
-		for (GraphElement elem : new GeoffStreamParser(inputStream)) {
-			if (elem instanceof Edge) {
-				createEdge((Edge) elem);
-			} else {
-				createNode((Node) elem);
-			}
-		}
-	}
+  public final void importNow() throws Exception {
+    for (GraphElement elem : new GeoffStreamParser(inputStream)) {
+      if (elem instanceof Edge) {
+        createEdge((Edge) elem);
+      } else {
+        createNode((Node) elem);
+      }
+    }
+  }
 
-	protected abstract void createEdge(Edge edge) throws Exception;
+  protected abstract void createEdge(Edge edge) throws Exception;
 
-	protected abstract void createNode(Node elem);
+  protected abstract void createNode(Node node);
 }
--- benchmark/src/main/java/info/gehrels/diplomarbeit/flockdb/FlockDBImporter.java
+++ benchmark/src/main/java/info/gehrels/diplomarbeit/flockdb/FlockDBImporter.java
@@ -8,80 +8,78 @@ import info.gehrels.diplomarbeit.Measurement;
 import info.gehrels.diplomarbeit.Node;
 import info.gehrels.flockDBClient.ExecutionBuilder;
 import info.gehrels.flockDBClient.FlockDB;
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Date;
-
 import static info.gehrels.diplomarbeit.Measurement.measure;
 import static info.gehrels.diplomarbeit.flockdb.FlockDBHelper.createFlockDB;
 import static info.gehrels.flockDBClient.Direction.OUTGOING;
 import static java.lang.Thread.sleep;
 import static java.sql.DriverManager.getConnection;
 
+
 public class FlockDBImporter extends AbstractImporter {
-	public static final String IMPORT_COMPLETED_SQL = "SELECT\n"
-	                                                  + "\tt1.cnt+\n"
-	                                                  + "\tt2.cnt+\n"
-	                                                  + "\tt3.cnt+\n"
-	                                                  + "\tt4.cnt\n"
-	                                                  + "FROM\n"
-	                                                  + "\t(SELECT COUNT(*) as cnt FROM edges_development.forward_1_0000_edges) AS t1,\n"
-	                                                  + "\t(SELECT COUNT(*) as cnt FROM edges_development.forward_2_0000_edges) AS t2,\n"
-	                                                  + "\t(SELECT COUNT(*) as cnt FROM edges_development.forward_3_0000_edges) AS t3,\n"
-	                                                  + "\t(SELECT COUNT(*) as cnt FROM edges_development.forward_4_0000_edges) AS t4";
+  public static final String IMPORT_COMPLETED_SQL = "SELECT\n" +
+    "\tt1.cnt+\n" +
+    "\tt2.cnt+\n" +
+    "\tt3.cnt+\n" +
+    "\tt4.cnt\n" +
+    "FROM\n" +
+    "\t(SELECT COUNT(*) as cnt FROM edges_development.forward_1_0000_edges) AS t1,\n" +
+    "\t(SELECT COUNT(*) as cnt FROM edges_development.forward_2_0000_edges) AS t2,\n" +
+    "\t(SELECT COUNT(*) as cnt FROM edges_development.forward_3_0000_edges) AS t3,\n" +
+    "\t(SELECT COUNT(*) as cnt FROM edges_development.forward_4_0000_edges) AS t4";
 
-	private final FlockDB flockDB;
+  private final FlockDB flockDB;
 
-	private int numberOfImportedEdges = 0;
+  private int numberOfImportedEdges = 0;
 
 
-	public static void main(final String... args) throws Exception {
-		measure(new Measurement<Void>() {
-			@Override
-			public void execute(Void database) throws Exception {
-				new FlockDBImporter(args[0]).importNow();
-			}
-		});
-	}
+  public static void main(final String... args) throws Exception {
+    measure(new Measurement<Void>() {
+        @Override
+        public void execute(Void database) throws Exception {
+          new FlockDBImporter(args[0]).importNow();
+        }
+      });
+  }
 
-	public FlockDBImporter(String sourceFile) throws Exception {
-		super(sourceFile);
-		flockDB = createFlockDB();
-	}
+  public FlockDBImporter(String sourceFile) throws Exception {
+    super(sourceFile);
+    flockDB = createFlockDB();
+  }
 
-	@Override
-	protected void createEdge(Edge edge) throws IOException, FlockException {
-		ExecutionBuilder executionBuilder = flockDB.batchExecution(Priority.High);
-		executionBuilder
-			.add(edge.from, Integer.valueOf(edge.label.substring(1)), new Date().getTime(), OUTGOING, edge.to);
-		executionBuilder.execute();
-		numberOfImportedEdges++;
-	}
+  @Override
+  protected void createEdge(Edge edge) throws IOException, FlockException {
+    ExecutionBuilder executionBuilder = flockDB.batchExecution(Priority.High);
+    executionBuilder.add(edge.from, Integer.valueOf(edge.label.substring(1)), new Date().getTime(), OUTGOING, edge.to);
+    executionBuilder.execute();
+    numberOfImportedEdges++;
+  }
 
-	@Override
-	protected void createNode(Node elem) {
-	}
+  @Override
+  protected void createNode(Node node) {
+  }
 
-	public void ensureImportCompleted() throws Exception {
-		System.err.println("ensureImportCompleted");
+  public void ensureImportCompleted() throws Exception {
+    System.err.println("ensureImportCompleted");
 
-		try (Connection con = getConnection("jdbc:mysql://localhost:3306/edges_development", "root", "")) {
-			for (long numOfEdges = 0; numOfEdges < numberOfImportedEdges; numOfEdges = getNumberOfImportedEdges(con)) {
-				System.err.println(numOfEdges + " edges completed yet, waiting...");
-				sleep(300);
-			}
-		}
-	}
+    try(Connection con = getConnection("jdbc:mysql://localhost:3306/edges_development", "root", "")) {
+      for (long numOfEdges = 0; numOfEdges < numberOfImportedEdges; numOfEdges = getNumberOfImportedEdges(con)) {
+        System.err.println(numOfEdges + " edges completed yet, waiting...");
+        sleep(300);
+      }
+    }
+  }
 
-	private long getNumberOfImportedEdges(Connection con) throws Exception {
-		try (Statement statement = con.createStatement();
-		     ResultSet resultSet = statement.executeQuery(IMPORT_COMPLETED_SQL)) {
-			resultSet.next();
-			return resultSet.getLong(1);
-		}
-	}
+  private long getNumberOfImportedEdges(Connection con) throws Exception {
+    try(Statement statement = con.createStatement();
+      ResultSet resultSet = statement.executeQuery(IMPORT_COMPLETED_SQL)) {
+      resultSet.next();
+      return resultSet.getLong(1);
+    }
+  }
 
 }
