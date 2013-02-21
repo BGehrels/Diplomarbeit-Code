f066399ede7bf2a6aa26bdd08955b343a986bf18
BGehrels <benjamin@gehrels>
2013-02-21 20:42:50 +0100

    Cleaning up the benchmark service log

--- benchmark/reset.sh
+++ benchmark/reset.sh
@@ -13,5 +13,7 @@ echo "DROP DATABASE edges_development" | mysql -u root
 echo "DROP DATABASE flockdb_development" | mysql -u root
 sudo service mysql stop
 sudo sh -c "rm /var/lib/mysql/ib*"
-
+sudo rm /var/log/upstart/benchmark.log
+sudo touch /var/log/upstart/benchmark.log
+sudo chmod 666 /var/log/upstart/benchmark.log
 bzip2 geoff/*.geoff
